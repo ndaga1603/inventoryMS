c1e2c709c965d29cf7425a30a3991842dfc9e8f1
SEMWANZA MIRAJI <semwanzamiraji@gmail.com>
2024-09-16 22:48:22 -0700

    first

--- inventoryApp/admin.py
+++ inventoryApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User,Customer,Product,Order,OrderProduct
+
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
 
 # Register your models here.
--- inventoryMS/settings.py
+++ inventoryMS/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # This is the static folder in the root directory
 ]
