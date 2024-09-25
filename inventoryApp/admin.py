597f5c0bd1ef3c93f3acf6296fc283ed758e2ba4
arafatyassir <131864319+arafatyassir@users.noreply.github.com>
2024-09-25 11:43:28 -0700

    some changes

--- inventoryApp/admin.py
+++ inventoryApp/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Customer, Order, Product, OrderProduct
-
-# Register your models here.
-admin.site.register([Customer, Order, Product, OrderProduct]);
-=======
 from .models import *
 
 
@@ -14,4 +8,3 @@ admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderProduct)
->>>>>>> 078c2d88e99555ac382a79e5984c9447c64fa914
